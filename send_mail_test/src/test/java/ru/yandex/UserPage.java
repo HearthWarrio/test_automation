4f7245e927b5c9e55d285303e1259556ee18d6ac
neksword <neksword@inbox.ru>
2020-10-09 15:49:50 +0300

    Update UserPage.java

--- send_mail_test/src/test/java/ru/yandex/UserPage.java
+++ send_mail_test/src/test/java/ru/yandex/UserPage.java
@@ -16,6 +16,8 @@ public class UserPage {
 
         this.driver = driver;
     }
+    //1. избавиться от таких путей и навсегда забыть о них. по такому пути невозможно найти человеку элемент, если вдруг что-то сломается. это очень ненадежный вариант и с точки
+    //зрения поддержки плохой. любой xpath можно описать по-другому, словами. 
     @FindBy(xpath = "//*[@id=\"nb-1\"]/body/div[2]/div[5]/div/div[2]/div/div/header/div/div[5]/div[2]/a[1]/div/img")
     private WebElement userMenu;
 
@@ -43,6 +45,7 @@ public class UserPage {
     //Для этого и подобного решения, мой комментарий: я прочитал о том, что не рекомендуется использовать прямые ссылки
     //по xpath. но, к сожалению, я не везде смог найти id/class/title. Поэтому, там, где их не нашел и использую
     //полные ссылки.
+    // скину статью, ты посмотришь - можно искать не только по ним, а практически по любому элемента дерева, плюс можно искать родителя/потомка относительно какого- элемента, что удобно.
     @FindBy(xpath = "//*[@id=\"nb-1\"]/body/div[2]/div[9]/div/div[2]/div/div[2]/div/div[1]/div[2]/div[1]/div/div[1]/div[1]/div/div[1]/div/div/div/div")
     private WebElement adressField;
 
@@ -127,7 +130,7 @@ public class UserPage {
         public String getUserName() {
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
-
+        // про xpath уже писал. второе: в метод лучше всегда стараться передать переменную с хорошим, понятным названием. потому что из этого вызова совершенно неясно, видимости какого элемента мы все-таки ожидаем?
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"nb-1\"]/body/div[2]/div[5]/div/div[2]/div/div/header/div/div[5]/div[2]/a[1]/span[1]")));
 
         String userName = userMenu.getText();
